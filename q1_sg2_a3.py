8f9be89fc9c9328920a8078751d76ada9b3e4a44
keisha <keishaabbiel@gmail.com>
2026-08-14 20:20:53 +0800

    Fix input handling for birth year in zodiac program

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -25,16 +25,19 @@ while True:
     #when user inputs an integer
     try:
         birth_year = int(input("Enter your birth year : "))
+
         #when user inputs an invalid integer
         if birth_year < 1900:
             print("\n\tInvalid year, it should not be earlier than 1900.")
             print("\tPlease enter your birth year again.\n")
+
         #when user inputs a valid integer
         else:
             index = (birth_year - 1900) % 12
             print("\n \tYour Chinese Zodiac Sign is :", zodiac[index])
             break
+
     #when user inputs a non-integer
     except ValueError:
         print("\n\tInvalid input, please enter a number.\n")
-  
+ 
