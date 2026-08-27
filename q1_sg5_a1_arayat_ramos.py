43d6817e980216294fd65beab7e1d69188611b75
keisha <keishaabbiel@gmail.com>
2026-08-27 09:21:54 +0800

    Refactor Hero class for health and damage handling
    Fix class initialization and damage calculation logic.

---
+++ q1_sg5_a1_arayat_ramos.py
@@ -0,0 +1,22 @@
+# to be continued
+
+'''
+hero
+- health (hp)
+
++ damage taken(attack)
++ new health(hp - attack)
+'''
+
+class Hero:
+    def __init__(hp, amount, take_damage):
+        self.hp == 100
+        self.amount = int(input("How much damage do you want to deal?"))
+        take_damage = self.take_damage(amount) - hp
+        
+        hero1 = "Arthur"
+        hero2 = "Morgana"
+        
+    def damage_done():
+        print("Arthur's health is at", take_damage)
+        print("Morgana's health is at", hp)
